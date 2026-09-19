5782271b2575a26a891ea9cf841dcb5ecca0aec3
olohunwaabdulsalam47-alt <olohunwaabdulsalam47@gmail.com>
2026-09-19 12:45:59 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBu37fV7AVatgXsA9YVbJQHELEMeCAfBhA",
