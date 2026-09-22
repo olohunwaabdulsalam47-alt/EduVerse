397e2b547812b0d10a28488ab26ba6c9b9556f95
olohunwaabdulsalam47-alt <olohunwaabdulsalam47@gmail.com>
2026-09-22 10:19:50 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Replace with YOUR Firebase config
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
